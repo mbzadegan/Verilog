f302b81a4c4a244194ac5f8e2104ab9cb2a6db6b
Mohammad BadieZadegan <mbzadegan@gmail.com>
2024-11-06 18:27:42 -0800

    Create 3x3_Matrix_Multiplication.v

---
+++ 3x3_Matrix_Multiplication.v
@@ -0,0 +1,25 @@
+module matrix_multiplication_3x3 (
+    input [15:0] A [0:2][0:2],  // 3x3 Matrix A
+    input [15:0] B [0:2][0:2],  // 3x3 Matrix B
+    output reg [31:0] C [0:2][0:2]  // 3x3 Result Matrix C
+);
+    integer i, j, k;
+
+    always @(*) begin
+        // Initialize the result matrix C to 0
+        for (i = 0; i < 3; i = i + 1) begin
+            for (j = 0; j < 3; j = j + 1) begin
+                C[i][j] = 0;
+            end
+        end
+
+        // Matrix multiplication
+        for (i = 0; i < 3; i = i + 1) begin
+            for (j = 0; j < 3; j = j + 1) begin
+                for (k = 0; k < 3; k = k + 1) begin
+                    C[i][j] = C[i][j] + (A[i][k] * B[k][j]);
+                end
+            end
+        end
+    end
+endmodule
